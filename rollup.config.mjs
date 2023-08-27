6e317d56175ff8f032d40420dcb355b33af9eb91
Braden Douglass <braden.douglass@gmail.com>
2023-08-26 23:27:22 -0400

    docs(:rollup) Cleanup Commented Out Code.
    Remove duplicate or unneeded code from this file.
    Really this tailors down the original boilerplate
    to something that can work with Propshaft.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,11 @@
-// Import rollup plugins
-// import html from '@web/rollup-plugin-html';
-// import {copy} from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-// import minifyHTML from 'rollup-plugin-minify-html-literals';
 import minifyLiterals from 'rollup-plugin-minify-html-literals-v3';
 import summary from 'rollup-plugin-summary';
 import path from 'path'
 
 export default {
   plugins: [
-    // Entry point for application build; can specify a glob to build multiple
-    // HTML files for non-SPA app
-    // html({
-    //   input: 'app/javscript/application.js',
-    // }),
     // Resolve bare module specifiers to relative paths
     resolve(),
     // Minify HTML template literals
@@ -27,10 +18,6 @@ export default {
     }),
     // Print bundle summary
     summary(),
-    // Optional: copy any static assets to build directory
-    // copy({
-    //   patterns: ['images/**/*'],
-    // }),
   ],
   input: path.join(process.cwd(), 'app/javascript/application.js'),
   output: {
